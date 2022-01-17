d084d58ed550650f17953fe4dc4c6f5808d1ebb2
Matheus <mrehbein45@gmail.com>
2022-01-17 17:21:34 -0300

    implementação enable e testes

--- application/product.go
+++ application/product.go
@@ -1,5 +1,7 @@
 package application
 
+import "errors"
+
 type ProductInterface interface {
 	IsValid() (bool, error)
 	Enable() error
@@ -22,24 +24,29 @@ type Product struct {
 	Price  float64
 }
 
-func (p *Product) isValid() (bool, error) {
+// func (p *Product) isValid() (bool, error) {
 
-}
+// }
 func (p *Product) Enable() error {
-
+	if p.Price > 0 {
+		p.Status = ENABLED
+		return nil
+	}
+	return errors.New("The price must be greater than zero to enable the product")
 }
-func (p *Product) Disable() error {
 
-}
+// func (p *Product) Disable() error {
+
+// }
 func (p *Product) GetID() string {
-
+	return p.ID
 }
 func (p *Product) GetName() string {
-
+	return p.Name
 }
 func (p *Product) GetStatus() string {
-
+	return p.Status
 }
 func (p *Product) GetPrice() float64 {
-
+	return p.Price
 }
