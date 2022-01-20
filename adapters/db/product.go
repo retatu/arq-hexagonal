d79493b3a2654ecbc3eeb56192e5ad4494a9e4da
Matheus <mrehbein45@gmail.com>
2022-01-20 18:19:20 -0300

    teste get

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -11,12 +11,16 @@ type ProductDB struct {
 	db *sql.DB
 }
 
+func NewProductDB(db *sql.DB) *ProductDB {
+	return &ProductDB{db: db}
+}
+
 // func (p *ProductDB) Save(product application.ProductInterface) (application.ProductInterface, error) {
 // }
 
 func (p *ProductDB) Get(id string) (application.ProductInterface, error) {
 	var product application.Product
-	stmt, err := p.db.Prepare("select id, name, price, status from products where id = ?")
+	stmt, err := p.db.Prepare("select id, name, price, status from products where id=?")
 	if err != nil {
 		return nil, err
 	}
--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -1,32 +1,48 @@
-package db
+package db_test
 
 import (
 	"database/sql"
 	"log"
+	"testing"
+
+	"github.com/retatu/arq-hexagonal/adapters/db"
+	"github.com/stretchr/testify/require"
 )
 
 var DB *sql.DB
 
 func setup() {
-	DB, _ sql.Open("sqlite3", ":memory:")
+	DB, _ = sql.Open("sqlite3", ":memory:")
 	createTable(DB)
 	createProduct(DB)
 }
 
-func createTable(db *sql.DB){
+func createTable(db *sql.DB) {
 	table := `create table products("id" string, "name" string, "price" float, "status" string)`
 	stmt, err := db.Prepare(table)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err.Error())
 	}
 	stmt.Exec()
 }
 
-func createProduct(db *sqlDB){
+func createProduct(db *sql.DB) {
 	insert := `insert into products values("abc", "Teste", 0, "disabled")`
 	stmt, err := db.Prepare(insert)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err.Error())
 	}
 	stmt.Exec()
+}
+
+func TestProductDb_Get(t *testing.T) {
+	setup()
+	defer DB.Close()
+
+	productDB := db.NewProductDB(DB)
+	product, err := productDB.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, 0.0, product.GetPrice())
+	require.Equal(t, "Teste", product.GetName())
+	require.Equal(t, "disabled", product.GetStatus())
 }
